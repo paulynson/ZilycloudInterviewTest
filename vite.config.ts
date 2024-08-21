162d7b751e1b83d18908ff9cfa1c155c2b37cb14
paulynson <paulabakada@gmail.com>
2024-08-21 14:36:17 +0100

    updated toml

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": "https://zam.zilytst.com/api/v1",
-    },
+    // proxy: {
+    //   "/api": "https://zam.zilytst.com/api/v1",
+    // },
   },
 });
